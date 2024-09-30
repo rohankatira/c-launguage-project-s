b87cb36359195c3f69f3cdf2b4ad3eca076356b5
rohankatira <rohankatira1@gmail.com>
2024-09-30 23:28:22 +0530

    done

---
+++ project-4/que-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main(){
+	
+	int i,j, num=11; 
+	
+	for(i=1; i<5; i++){  
+		
+		for(j=1; j<=i; j++){  
+			
+			printf("%d ",num);  
+			num++;  
+		}
+		
+		printf("\n");  
+	}
+}
